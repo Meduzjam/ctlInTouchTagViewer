bbf9a1aee853840a669673398cba83e76265a592
Макаров Александр Сергеевич <makarovas@tms-samara.ru>
2019-12-18 10:37:51 +0400

    Добавьте файлы проекта.

---
+++ ctlInTouchTagViewer/ctlInTouchTagViewer.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ctlInTouchTagViewer
+{
+    partial class ctlInTouchTagViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lbGroup = new System.Windows.Forms.ListBox();
+            this.lvTags = new System.Windows.Forms.ListView();
+            this.Tag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(3, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lbGroup);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvTags);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint_1);
+            this.splitContainer1.Size = new System.Drawing.Size(822, 503);
+            this.splitContainer1.SplitterDistance = 274;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // lbGroup
+            // 
+            this.lbGroup.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbGroup.FormattingEnabled = true;
+            this.lbGroup.Location = new System.Drawing.Point(3, 3);
+            this.lbGroup.Name = "lbGroup";
+            this.lbGroup.Size = new System.Drawing.Size(268, 498);
+            this.lbGroup.TabIndex = 0;
+            this.lbGroup.SelectedIndexChanged += new System.EventHandler(this.lbGroup_SelectedIndexChanged_1);
+            // 
+            // lvTags
+            // 
+            this.lvTags.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Tag,
+            this.Name});
+            this.lvTags.GridLines = true;
+            this.lvTags.HideSelection = false;
+            this.lvTags.Location = new System.Drawing.Point(3, 3);
+            this.lvTags.Name = "lvTags";
+            this.lvTags.Size = new System.Drawing.Size(538, 497);
+            this.lvTags.TabIndex = 0;
+            this.lvTags.UseCompatibleStateImageBehavior = false;
+            this.lvTags.View = System.Windows.Forms.View.Details;
+            // 
+            // Tag
+            // 
+            this.Tag.Text = "Тэг";
+            this.Tag.Width = 200;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Наименование";
+            this.Name.Width = 300;
+            // 
+            // ctlInTouchTagViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "ctlInTouchTagViewer";
+            this.Size = new System.Drawing.Size(828, 507);
+            this.Load += new System.EventHandler(this.ctlInTouchTagViewer_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lbGroup;
+        private System.Windows.Forms.ListView lvTags;
+        private System.Windows.Forms.ColumnHeader Tag;
+        private System.Windows.Forms.ColumnHeader Name;
+    }
+}
---
+++ ctlInTouchTagViewer/ctlInTouchTagViewer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ctlInTouchTagViewer
+{
+    public partial class ctlInTouchTagViewer: UserControl
+    {
+
+        private Microsoft.VisualBasic.FileIO.TextFieldParser parser;
+        private List<string> ltags;
+        private String srcFile = @"D:\WORK\2017\ПСП Михайловская\export.csv";
+        private bool simpleMode;
+
+
+        
+        public String SrcFile
+        {
+            // Retrieves the value of the private variable colBColor.
+            get
+            {
+                return srcFile;
+            }
+            // Stores the selected value in the private variable colBColor, and
+            // updates the background color of the label control lblDisplay.
+            set
+            {
+                srcFile = value;
+            }
+        }
+
+        public bool SimpleMode
+        {
+            // Retrieves the value of the private variable colBColor.
+            get
+            {
+                return simpleMode;
+            }
+            // Stores the selected value in the private variable colBColor, and
+            // updates the background color of the label control lblDisplay.
+            set
+            {
+                simpleMode = value;
+            }
+        }
+
+        public ctlInTouchTagViewer()
+        {
+            InitializeComponent();
+            ltags = new List<string>();
+
+
+        }
+
+
+        private void ctlInTouchTagViewer_Load(object sender, EventArgs e)
+        {
+         
+
+            int ttype = 0;
+            List<string> tlist = new List<string>();
+            try
+            {
+                parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(srcFile,System.Text.Encoding.Default);
+                parser.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited;
+                parser.SetDelimiters(new string[] { ";" });
+
+                while (!parser.EndOfData)
+                {
+                    string[] row = parser.ReadFields();
+
+                    if (row[0].StartsWith(":IODisc"))
+                    { //18-1
+                        ttype = 1;
+                        continue;
+                    }
+                    else
+                    if (row[0].StartsWith(":IOReal"))
+                    { //47-1
+                        ttype = 2;
+                        continue;
+                    }
+                    else
+                    if (row[0].StartsWith(":IOInt"))
+                    { //46-1
+                        ttype = 3;
+                        continue;
+                    }
+                    else
+                    if (row[0].StartsWith(":"))
+                    {
+                        ttype = 0;
+                    }
+
+                    if (ttype == 0)
+                        continue;
+
+
+                    string s = row[17 * Convert.ToInt32(ttype == 1) + 46 * Convert.ToInt32(ttype == 2) + 45 * Convert.ToInt32(ttype == 3)];
+                    
+                    if (s.IndexOf(":") < 0)
+                        continue;
+
+                    ltags.Add(s);
+
+                    s = s.Substring(0, s.IndexOf(":")+1);
+
+                    if (!tlist.Exists(   delegate(String text) { return text == s; } ))                      
+                        tlist.Add(s);
+
+                    
+
+                }
+            }
+            
+            catch (System.Exception ex)
+            {
+                 MessageBox.Show(ex.Message);
+            }
+
+            
+            
+            foreach (string s in tlist)
+                lbGroup.Items.Add(s.Substring(0,s.Length-1));
+
+
+        }
+
+
+        private void lbGroup_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (lbGroup.SelectedItems.Count == 0)
+            {
+                lvTags.Clear();
+                return;
+            }
+
+
+
+            foreach (string s in ltags.FindAll(delegate (String text) { return text.StartsWith(lbGroup.SelectedItems[0] + ":"); }))
+                lvTags.Items.Add(s);
+        }
+    }
+}
